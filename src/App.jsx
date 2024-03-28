b22fb49c93aa0d556404903f60f0f78bea3c7116
Bhaverohit <rohitrkade@gmail.com>
2024-03-28 19:21:34 +0530

    Password edit and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <Manager />
+      <div className='bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+        <Manager />
+      </div>
       <Footer />
     </div>
   )
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Bounce, ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import { v4 as uuidv4, v4 } from 'uuid';
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -29,10 +30,40 @@ const Manager = () => {
     }
 
     const savePassword = () => {
-        setPasswordArray([...passwordArray, form]);
-        localStorage.setItem("password", JSON.stringify([...passwordArray, form]));
-        console.log(passwordArray);
+        if (form.site.length > 3 && form.username.length > 3 && form.password.length > 3) {
+
+            setPasswordArray([...passwordArray, { ...form, id: uuidv4() }]);
+            localStorage.setItem("password", JSON.stringify([...passwordArray, { ...form, id: uuidv4() }]));
+            setForm({
+                site: "",
+                username: "",
+                password: ""
+            });
+            console.log(passwordArray);
+        }
+        else {
+            // FIXME: Create feature to show error on empty fields & less characters
+            // FIXME: add a toast here {Error : Can't save empty password}
+        }
+
     }
+
+
+    const handleEdit = (id) => {
+        // BUG : Clicking on edit button multiple times removing all passwords available
+        // FEATURE: Create a confirm component which asks before deleting a password
+        console.log("Editing ", id);
+        setForm(passwordArray.filter(i => i.id === id)[0])
+        setPasswordArray(passwordArray.filter(item => item.id !== id))
+    }
+
+
+    const handleDelete = (id) => {
+        console.log("Deleting ", id);
+        setPasswordArray(passwordArray.filter(item => item.id !== id));
+        localStorage.setItem("password", JSON.stringify(passwordArray.filter(item => item.id !== id)));
+    }
+
 
     const handleChange = (e) => {
         e.preventDefault();
@@ -76,7 +107,7 @@ const Manager = () => {
 
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-900 opacity-20 blur-[100px]"></div></div>
 
-            <div className="my_container">
+            <div className="p-2 md:p-0 md:my_container min-h-[86.2vh]">
 
                 <h1 className='text-5xl font-bold text text-center'>
 
@@ -88,7 +119,7 @@ const Manager = () => {
 
                 <div className="flex flex-col p-4 text-black gap-8 items-center">
                     <input placeholder='Enter Website URL' onChange={handleChange} value={form.site} type="text" className='rounded-full border border-green-500 w-full py-1 p-4' id='site' name='site' />
-                    <div className='flex w-full justify-between gap-8'>
+                    <div className='flex flex-col md:flex-row w-full justify-between gap-8'>
                         <input placeholder='Enter Username' onChange={handleChange} value={form.username} type="text" className='rounded-full border border-green-500 w-full py-1 p-4' id='username' name='username' />
                         <div className="relative">
 
@@ -116,12 +147,13 @@ const Manager = () => {
 
                     {passwordArray.length === 0 && <div className='text-center'>No Passwords To Show</div>}
                     {passwordArray.length !== 0 &&
-                        <table className='table-auto w-full text-center rounded-xl overflow-hidden'>
+                        <table className='table-auto w-full text-center rounded-xl overflow-hidden mb-10'>
                             <thead className='bg-green-800 text-white'>
                                 <tr className='font-bold'>
                                     <th className='py-1.5'>Website</th>
                                     <th className='py-1.5'>Username</th>
                                     <th className='py-1.5'>Password</th>
+                                    <th className='py-1.5'>Actions</th>
                                 </tr>
                             </thead>
                             <tbody className='bg-green-100'>
@@ -149,6 +181,12 @@ const Manager = () => {
                                                 <div className='size-7 hover:cursor-pointer p-1' onClick={() => copyText(item.password, "Password")}>
                                                     <img src="src/assets/copy.png" alt="copy" />
                                                 </div>
+                                            </div>
+                                        </td>
+                                        <td className='py-1.5 text-green-950 border border-white text-center'>
+                                            <div className='flex justify-center items-center gap-3'>
+                                                <span onClick={() => handleEdit(item.id)}><img src="src/assets/edit.png" alt="edit" width={20} /></span>
+                                                <span onClick={() => handleDelete(item.id)}><img src="src/assets/remove.png" alt="delete" width={20} /></span>
                                             </div>
                                         </td>
                                     </tr>
